73de6a01076ed315d2ecb0a1a784b50c01dcee67
adamtech <adamtech323@gmail.com>
2022-02-04 21:36:00 +0800

    sc

--- limit-speed.sh
+++ limit-speed.sh
@@ -4,7 +4,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
-IZIN=$( curl https://raw.githubusercontent.com/alifnurmareta/premium/main/iptext.sh | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/CODETRCK/premium/main/iptext.sh | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 clear
 echo -e "${green} Please Wait, Proses...${NC}"
--- ohp.sh
+++ ohp.sh
@@ -37,7 +37,7 @@ Port_ssh='8383';
 
 # Installing ohp Server
 cd
-wget -O /usr/local/bin/ohp "https://raw.githubusercontent.com/alifnurmareta/bc/main/ohp/ohp"
+wget -O /usr/local/bin/ohp "https://raw.githubusercontent.com/CODETRCK/NEW-SC/main/ohp/ohp"
 chmod +x /usr/local/bin/ohp
 
 # Buat File OpenVPN TCP OHP
